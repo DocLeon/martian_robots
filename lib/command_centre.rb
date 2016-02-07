2b78400600049ab8f0a29e6d798a3a612288bbd4
Leon Hewitt <leon@Leons-MacBook-Pro.local>
2016-02-07 20:20:52 +0000

    refactor orientation

--- lib/command_centre.rb
+++ lib/command_centre.rb
@@ -1,27 +1,4 @@
-class Position
-  attr_reader :x_coordinate
-  attr_reader :y_coordinate
-  def self.from(start_position,movements)
-    Position.new(start_position, movements)
-  end
-
-  def initialize(co_ords, movements)
-    @x_coordinate = co_ords[0]
-    @y_coordinate = co_ords[1]
-    @orientation = move(movements)
-  end
-
-  def move(movement)
-    return "S" if movement == "R"
-    return "N" if movement == "L"
-    return "W" if movement == "RR"
-    "E"
-  end
-
-  def to_s
-    "#{@x_coordinate} #{@y_coordinate} #{@orientation}"
-  end
-end
+require 'Position'
 
 class CommandCentre
   def send(instructions_sent)
--- spec/command_centre_spec.rb
+++ spec/command_centre_spec.rb
@@ -19,21 +19,21 @@ describe CommandCentre do
     end
 
     context '1 robot in fleet, receives L command' do
-      it 'reports back robot is facing south' do
+      it 'reports back robot is facing north' do
         command_centre.send([["5 3"],["1 1 E"],["L"]])
         expect(command_centre.output).to eq("1 1 N")
       end
     end
 
     context '1 robot in fleet, receives LR command' do
-      it 'reports back robot is facing south' do
+      it 'reports back robot is facing east' do
         command_centre.send([["5 3"],["1 1 E"],["LR"]])
         expect(command_centre.output).to eq("1 1 E")
       end
     end
 
     context '1 robot in fleet, receives RR command' do
-      it 'reports back robot is facing south' do
+      it 'reports back robot is facing west' do
         command_centre.send([["5 3"],["1 1 E"],["RR"]])
         expect(command_centre.output).to eq("1 1 W")
       end

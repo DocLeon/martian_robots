cf39760db95b7a0cb0ef6166830449eb22152128
Leon Hewitt <leon@Leons-MacBook-Pro.local>
2016-02-07 20:27:21 +0000

    all rotations for all orientations

---
+++ lib/Position.rb
@@ -0,0 +1,40 @@
+class Position
+  attr_reader :x_coordinate
+  attr_reader :y_coordinate
+  def self.from(start_position,movements)
+    Position.new(start_position, movements)
+  end
+
+  def initialize(co_ords, movements)
+    @x_coordinate = co_ords[0]
+    @y_coordinate = co_ords[1]
+    @original_orientation = co_ords[2]
+    @orientation = move(movements)
+  end
+
+  def move(movement)
+    return orientation_from(@original_orientation, movement)
+  end
+
+  def orientation_from(original_orientation, movement)
+    compass_points = ['N','E','S','W']
+    final_orientation = original_orientation
+    movement.each_char do |movement|
+      adjustment = orientation_adjustment_from movement
+      final_compass_index = compass_points.index(final_orientation) + adjustment
+      final_compass_index = 0 if final_compass_index > 3
+      final_orientation = compass_points[final_compass_index]
+    end
+    final_orientation
+  end
+
+  def orientation_adjustment_from(movement)
+    return 1 if movement == "R"
+    return -1 if movement == "L"
+    0
+  end
+
+  def to_s
+    "#{@x_coordinate} #{@y_coordinate} #{@orientation}"
+  end
+end
---
+++ spec/position_spec.rb
@@ -0,0 +1,21 @@
+require 'spec_helper'
+
+describe Position do
+  describe '#to_s' do
+    it "is orientated correctly when rotated" do
+      [['N','R','E'],
+       ['N','L','W'],
+       ['E','R','S'],
+       ['E','L','N'],
+       ['S','R','W'],
+       ['S','L','E'],
+       ['W','R','N'],
+       ['W','L','S']]
+      .each do |start_orientation,movement,end_orientation|
+        position = Position.new(['1','1',start_orientation],movement)
+        expect(position.to_s).to eq("1 1 #{end_orientation}")
+      end
+
+    end
+  end
+end

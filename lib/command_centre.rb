2ca0f98cf99c4033e166bc896a8fb48efa2e9952
Leon Hewitt <leon@Leons-MacBook-Pro.local>
2016-02-07 19:17:02 +0000

    command centre responsible for interpreting instructions sent

--- lib/command_centre.rb
+++ lib/command_centre.rb
@@ -1,8 +1,8 @@
 class Position
   attr_reader :x_coordinate
   attr_reader :y_coordinate
-  def self.from(instructions_sent)
-    Position.new(instructions_sent[1][0].split(' '), instructions_sent[2][0])
+  def self.from(start_position,movements)
+    Position.new(start_position, movements)
   end
 
   def initialize(co_ords, movements)
@@ -24,12 +24,14 @@ class Position
 end
 
 class CommandCentre
-  def send(instructions)
-    @final_position = Position.from(instructions)
+  def send(instructions_sent)
+    start_position = instructions_sent[1][0].split(' ')
+    movements = instructions_sent[2][0]
+    @final_position = Position.from(start_position,movements)
   end
 
   def output
-    return "#{@final_position}"
+    "#{@final_position}"
   end
 
 end

825d182c7202d30d89b829f022efce864d00c28f
Leon Hewitt <leon@Leons-MacBook-Pro.local>
2016-02-07 19:13:24 +0000

    move logic into position class

--- lib/command_centre.rb
+++ lib/command_centre.rb
@@ -2,38 +2,34 @@ class Position
   attr_reader :x_coordinate
   attr_reader :y_coordinate
   def self.from(instructions_sent)
-    Position.new(instructions_sent[1][0].split(' '))
+    Position.new(instructions_sent[1][0].split(' '), instructions_sent[2][0])
   end
 
-  def initialize(co_ords)
+  def initialize(co_ords, movements)
     @x_coordinate = co_ords[0]
     @y_coordinate = co_ords[1]
+    @orientation = move(movements)
   end
 
-  def to_s
-    @x_coordinate + ' ' + @y_coordinate
-  end
-end
-
-class CommandCentre
-  def send(instructions)
-    @instructions = instructions
-  end
-
-  def output
-    movement = @instructions[2][0]
-    position = Position.from(@instructions)
-    return "#{position} #{position_after_movement(movement)}"
-  end
-
-  def movement_from(instuctions)
-    instructions[2][0]
-  end
-
-  def position_after_movement(movement)
+  def move(movement)
     return "S" if movement == "R"
     return "N" if movement == "L"
     return "W" if movement == "RR"
     "E"
   end
+
+  def to_s
+    "#{@x_coordinate} #{@y_coordinate} #{@orientation}"
+  end
+end
+
+class CommandCentre
+  def send(instructions)
+    @final_position = Position.from(instructions)
+  end
+
+  def output
+    return "#{@final_position}"
+  end
+
 end

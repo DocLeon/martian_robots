3020dbb358cb7dc18134903f3be70dc6265eb7e0
Leon Hewitt <leon@Leons-MacBook-Pro.local>
2016-02-08 10:57:37 +0000

    move forward - simple case

--- lib/Position.rb
+++ lib/Position.rb
@@ -20,12 +20,20 @@ class Position
     compass_points = ['N','E','S','W']
     final_orientation = original_orientation
     movement.each_char do |movement|
+      move_forward(final_orientation) if movement == 'F'
       adjustment = orientation_adjustment_from movement
       final_compass_index = compass_points.index(final_orientation) + adjustment
       final_compass_index = 0 if final_compass_index > 3
       final_orientation = compass_points[final_compass_index]
     end
     final_orientation
+  end
+
+  def move_forward(orientation)
+    @y_coordinate = (@y_coordinate.to_i + 1).to_s if orientation == 'N'
+    @y_coordinate = (@y_coordinate.to_i - 1).to_s if orientation == 'S'
+    @x_coordinate = (@x_coordinate.to_i - 1).to_s if orientation == 'W'
+    @x_coordinate = (@x_coordinate.to_i + 1).to_s if orientation == 'E'
   end
 
   def orientation_adjustment_from(movement)
--- spec/position_spec.rb
+++ spec/position_spec.rb
@@ -16,6 +16,16 @@ describe Position do
         position = Position.new(['1','1',start_orientation],movement)
         expect(position.to_s).to eq("1 1 #{end_orientation}")
       end
+    end
+    it "from 1 1 it moves forward in orientation when instructed" do
+      [['N','1 2 N'],
+       ['S','1 0 S'],
+       ['W','0 1 W'],
+       ['E','2 1 E']]
+      .each do |start_orientation,final_position|
+        position = Position.new(['1','1',start_orientation],'F')
+        expect(position.to_s).to eq(final_position)
+      end
 
     end
   end

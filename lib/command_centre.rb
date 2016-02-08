224cfbbd2bae6efd8033d3e74cc6cfc102273708
Leon Hewitt <leon@Leons-MacBook-Pro.local>
2016-02-08 21:11:23 +0000

    multi robot

--- lib/command_centre.rb
+++ lib/command_centre.rb
@@ -2,14 +2,30 @@ require 'robot_position'
 
 class CommandCentre
   def send(instructions_sent)
-    start_position = instructions_sent[1][0].split(' ')
-    movements = instructions_sent[2][0]
-    @robot_position = RobotPosition.new(start_position)
-    @robot_position.move(movements)
+    robot_instructions_start = 1
+    @robot_positions = []
+     while robot_instructions_start < instructions_sent.count do
+       instructions = instructions_sent[robot_instructions_start,2]
+       robot_position = final_robot_position_from instructions
+       @robot_positions << robot_position
+       robot_instructions_start += 2
+     end
+  end
+
+  def final_robot_position_from(instructions)
+    start_position = instructions[0][0].split(' ')
+    movements = instructions[1][0]
+    robot_position = RobotPosition.new(start_position)
+    robot_position.move(movements)
+    robot_position
   end
 
   def output
-    [["#{@robot_position}"]]
+    output = []
+    @robot_positions.each do |robot_position|
+      output << ["#{robot_position}"]
+    end
+    output
   end
 
 end
--- spec/command_centre_spec.rb
+++ spec/command_centre_spec.rb
@@ -4,41 +4,42 @@ describe CommandCentre do
   subject(:command_centre) { CommandCentre.new }
 
   describe '#send' do
-    context '1 robot in fleet, no commands' do
+    context '1 robot in fleet' do
       context 'no commands' do
         it  'reports back position of robot' do
           command_centre.send([["5 3"],["1 1 E"],[""]])
           expect(command_centre.output).to eq([["1 1 E"]])
         end
+        context 'receives R command' do
+          it 'reports back robot is facing south' do
+            command_centre.send([["5 3"],["1 1 E"],["R"]])
+            expect(command_centre.output).to eq([["1 1 S"]])
+          end
+        end
+        context 'receives L command' do
+          it 'reports back robot is facing north' do
+            command_centre.send([["5 3"],["1 1 E"],["L"]])
+            expect(command_centre.output).to eq([["1 1 N"]])
+          end
+        end
+        context 'receives LR command' do
+          it 'reports back robot is facing east' do
+            command_centre.send([["5 3"],["1 1 E"],["LR"]])
+            expect(command_centre.output).to eq([["1 1 E"]])
+          end
+        end
+        context 'receives RR command' do
+          it 'reports back robot is facing west' do
+            command_centre.send([["5 3"],["1 1 E"],["RR"]])
+            expect(command_centre.output).to eq([["1 1 W"]])
+          end
+        end
       end
-
-    end
-
-    context '1 robot in fleet, receives R command' do
-      it 'reports back robot is facing south' do
-        command_centre.send([["5 3"],["1 1 E"],["R"]])
-        expect(command_centre.output).to eq([["1 1 S"]])
-      end
-    end
-
-    context '1 robot in fleet, receives L command' do
-      it 'reports back robot is facing north' do
-        command_centre.send([["5 3"],["1 1 E"],["L"]])
-        expect(command_centre.output).to eq([["1 1 N"]])
-      end
-    end
-
-    context '1 robot in fleet, receives LR command' do
-      it 'reports back robot is facing east' do
-        command_centre.send([["5 3"],["1 1 E"],["LR"]])
-        expect(command_centre.output).to eq([["1 1 E"]])
-      end
-    end
-
-    context '1 robot in fleet, receives RR command' do
-      it 'reports back robot is facing west' do
-        command_centre.send([["5 3"],["1 1 E"],["RR"]])
-        expect(command_centre.output).to eq([["1 1 W"]])
+      context 'many robots in fleet' do
+        it 'reports back for many robots' do
+          command_centre.send([["5 3"],["1 1 E"],["LR"],["1 1 E"],["LR"]])
+          expect(command_centre.output.count).to eq(2)
+        end
       end
     end
   end

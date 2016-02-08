ad09c9265f50e7f8ff5c6da629357b6bf994ecd2
Leon Hewitt <leon@Leons-MacBook-Pro.local>
2016-02-08 20:25:17 +0000

    fix failing spec and tidy up spec code

--- spec/command_centre_spec.rb
+++ spec/command_centre_spec.rb
@@ -5,37 +5,40 @@ describe CommandCentre do
 
   describe '#send' do
     context '1 robot in fleet, no commands' do
-      it  'reports back position of robot' do
-        command_centre.send([["5 3"],["1 1 E"],[""]])
-        expect(command_centre.output).to eq(["1 1 E"])
+      context 'no commands' do
+        it  'reports back position of robot' do
+          command_centre.send([["5 3"],["1 1 E"],[""]])
+          expect(command_centre.output).to eq([["1 1 E"]])
+        end
       end
+
     end
 
     context '1 robot in fleet, receives R command' do
       it 'reports back robot is facing south' do
         command_centre.send([["5 3"],["1 1 E"],["R"]])
-        expect(command_centre.output).to eq(["1 1 S"])
+        expect(command_centre.output).to eq([["1 1 S"]])
       end
     end
 
     context '1 robot in fleet, receives L command' do
       it 'reports back robot is facing north' do
         command_centre.send([["5 3"],["1 1 E"],["L"]])
-        expect(command_centre.output).to eq(["1 1 N"])
+        expect(command_centre.output).to eq([["1 1 N"]])
       end
     end
 
     context '1 robot in fleet, receives LR command' do
       it 'reports back robot is facing east' do
         command_centre.send([["5 3"],["1 1 E"],["LR"]])
-        expect(command_centre.output).to eq(["1 1 E"])
+        expect(command_centre.output).to eq([["1 1 E"]])
       end
     end
 
     context '1 robot in fleet, receives RR command' do
       it 'reports back robot is facing west' do
         command_centre.send([["5 3"],["1 1 E"],["RR"]])
-        expect(command_centre.output).to eq(["1 1 W"])
+        expect(command_centre.output).to eq([["1 1 W"]])
       end
     end
   end
